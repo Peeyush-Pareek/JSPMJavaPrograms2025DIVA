774f49f06c4aa3eff5e1b27038ffc7f4e67d5647
sohamkadam05 <sohamkadam9547@gmail.com>
2025-09-04 10:16:50 +0530

    Add files via upload

---
+++ Swapnumberswithouttemp.java
@@ -0,0 +1,16 @@
+public class Swapnumberswithouttemp {
+    public static void main(String[] args) {
+        int a = 10;  
+        int b = 50;  
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        a = a + b;  
+        b = a - b;  
+        a = a - b; 
+
+        System.out.println("\nAfter swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
